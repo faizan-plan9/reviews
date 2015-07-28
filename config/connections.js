3b7b2a9adad8197f6e573cea28957e37eab15261
ahsangondal <ahsangondal15@gmail.com>
2015-07-28 11:53:24 +0500

    login page

--- config/connections.js
+++ config/connections.js
@@ -56,13 +56,13 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
+  mongo: {
     adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    host: 'ds063449.mongolab.com',
+    port: 63449,
+    user: 'ahsan',
+    password: 'ahsan',
+    database: 'heroku_93mg8jmk'
   },
 
   /***************************************************************************
