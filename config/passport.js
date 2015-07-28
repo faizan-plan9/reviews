c5dcc6b0a39ef133d223099a34289fcd49dba30c
ahsangondal <ahsangondal15@gmail.com>
2015-07-28 12:51:52 +0500

    hello

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,31 @@ var _super = require('sails-permissions/config/passport');
 
 _.merge(exports, _super);
 _.merge(exports, {
-
+  
   // Extend with custom logic here by adding additional fields, methods, etc.
+  
 
 });
+module.exports.passport = {
+
+local: {
+    strategy: require('passport-local').Strategy
+  },
+
+  basic: {
+    strategy: require('passport-http').BasicStrategy,
+    protocol: 'basic'
+  },
+
+  facebook: {
+    name: 'Facebook',
+    protocol: 'oauth2',
+    strategy: require('passport-facebook').Strategy,
+    options: {
+      clientID: '436918573176696',
+      clientSecret: 'acfc7bd6a5eb4eecbd684ce4bd249a13'
+      
+    }
+  }
+
+};
