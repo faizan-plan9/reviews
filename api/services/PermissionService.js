b0865817f4a15418d0732759128873d1730981ae
ahsangondal <ahsangondal15@gmail.com>
2015-07-29 23:35:14 +0500

    yeah baby yeah

--- api/services/PermissionService.js
+++ api/services/PermissionService.js
@@ -11,6 +11,28 @@ _.extend(PermissionService.prototype, {
   // Extend with custom logic here by adding additional fields and methods,
   // and/or overriding methods in the superclass.
 
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
 });
 
 module.exports = new PermissionService();
--- config/policies.js
+++ config/policies.js
@@ -36,6 +36,9 @@ module.exports.policies = {
   },
   TestController:{
     '*': true
+  },
+  UserController:{
+    'create':true
   }
 
 
